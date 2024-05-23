7c8f4aa0abc4392899af00f10d55c837147f1210
Dan Cristian Deac <79625820+dandeac26@users.noreply.github.com>
2024-05-23 14:11:14 +0300

    add clients script

--- scripts/InitBasicProductsList.py
+++ scripts/InitBasicProductsList.py
@@ -5,6 +5,9 @@ import requests
 with open("products.json") as f:
     products = json.load(f)
 
+with open("clients.json") as f:
+    clients = json.load(f)
+
 # For each product, make a POST request to the API endpoint
 for product in products:
     response = requests.post("http://localhost:8080/product", json=product)
@@ -13,4 +16,13 @@ for product in products:
     if response.status_code != 201:
         print(
             f'Failed to create product {product["name"]}. Status code: {response.status_code}'
+        )
+
+for client in clients:
+    response = requests.post("http://localhost:8080/client", json=client)
+
+    # Check the response
+    if response.status_code != 201:
+        print(
+            f'Failed to create client {client["firmName"]}. Status code: {response.status_code}'
         )
